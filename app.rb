cf5bc007d2f66ce0a3063deda08ad82c6eed0545
Christopher Leja <christopher.leja@gmail.com>
2020-03-09 20:42:32 +0000

    Automatically backed up by Learn

--- app.rb
+++ app.rb
@@ -3,13 +3,11 @@ require_relative 'config/environment'
 class App < Sinatra::Base
   
   get '/reversename/:name' do
-    @name = params[:name]
-    "#{@name}".reverse
+    "#{params[:name]}".reverse
   end
   
   get '/square/:number'do 
-    @number = params[:number]
-    "#{@number.to_i * @number.to_i}"
+    "#{params[:number] ** 2}"
   end
   
   get '/say/:number/:phrase' do
@@ -21,12 +19,7 @@ class App < Sinatra::Base
   end
   
   get '/say/:word1/:word2/:word3/:word4/:word5' do
-    @word1 = params[:word1]
-    @word2 = params[:word2]
-    @word3 = params[:word3]
-    @word4 = params[:word4]
-    @word5 = params[:word5]
-    "#{@word1} #{@word2} #{@word3} #{@word4} #{@word5}."
+    "#{params[:word1]} #{params[:word2]} #{params[:word3]} #{params[:word4]} #{params[:word5]}."
   end
   
   get '/:operation/:number1/:number2' do
