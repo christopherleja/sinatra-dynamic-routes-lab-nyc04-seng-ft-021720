43c4f5ceef364aaba54e9de7dfd2979ec1b8d06a
Christopher Leja <christopher.leja@gmail.com>
2020-03-09 19:57:10 +0000

    Automatically backed up by Learn

--- app.rb
+++ app.rb
@@ -7,7 +7,9 @@ class App < Sinatra::Base
     "#{@name}".reverse
   end
   
-  get '/'do 
+  get '/square/:number'do 
+    @number = params[:number]
+    "#{@number.to_i * @number.to_i}"
   end
 
 end
