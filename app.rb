7d56d1d1ff421b5fc7d5bfdc950f32039612d73e
Christopher Leja <christopher.leja@gmail.com>
2020-03-09 20:02:25 +0000

    Automatically backed up by Learn

--- app.rb
+++ app.rb
@@ -15,7 +15,7 @@ class App < Sinatra::Base
   get '/say/:number/:phrase' do
     @phrase = params[:phrase]
     @number = params[:number]
-    answer = @number.to_i.times do "#{@phrase}"
+    answer = @number.to_i.times do puts @phrase
     "#{answer}".concat
     end
   end
