db9e79c9cf26adbf9df0a7fc5d6c0bc69e9ce1b2
Christopher Leja <christopher.leja@gmail.com>
2020-03-09 20:16:12 +0000

    Automatically backed up by Learn

--- app.rb
+++ app.rb
@@ -29,6 +29,10 @@ class App < Sinatra::Base
     "#{@word1} #{@word2} #{@word3} #{@word4} #{@word5}."
   end
   
-  get '/' do
+  get '/:operation/:number1/:number2' do
+    @operation = params[:operation]
+    @number1 = params[:number1]
+    @number2 = params[:number2]
+      "#{@number1}.to_i #{@operation} #{@number2}.to_i"
   end
 end
