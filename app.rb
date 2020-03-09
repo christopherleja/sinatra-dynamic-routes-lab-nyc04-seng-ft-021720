a5cf460764c97d85fab310c85ddb234880489361
Christopher Leja <christopher.leja@gmail.com>
2020-03-09 20:32:49 +0000

    Automatically backed up by Learn

--- app.rb
+++ app.rb
@@ -34,7 +34,7 @@ class App < Sinatra::Base
     @operation = params[:operation]
     @number1 = params[:number1]
     @number2 = params[:number2]
-      case
+      case @operation
       when @operation == "add"
         @operation = +
       elsif @operation == "multiply"
